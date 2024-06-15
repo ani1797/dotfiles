4d73715c9f2cb0e1d9c82dd785f1c3c70b74ffa1
Anirudh Aggarwal <ani1797@users.noreply.github.com>
2024-06-15 18:26:51 +0000

    chore: Update install.sh to include vi in the list of installed packages
    The install.sh script has been updated to include the `vi` package in the list of packages installed using `apt-get`. This ensures that `vi` is available for use in the system.
    
    Signed-off-by: GitHub <noreply@github.com>

--- preinstall.sh
+++ preinstall.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install git curl gcc ca-certificates zsh -y -qq --no-install-recommends
+apt-get update && apt-get install git curl gcc ca-certificates zsh vi -y -qq --no-install-recommends

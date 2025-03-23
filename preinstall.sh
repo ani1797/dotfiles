e2708927071a3da8c6aa64817403720128b5a0aa
Anirudh Aggarwal <starfire1797@gmail.com>
2025-03-23 13:09:11 -0400

    chore: removing nano.
    Signed-off-by: Anirudh Aggarwal <starfire1797@gmail.com>

--- preinstall.sh
+++ preinstall.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install git curl gcc ca-certificates zsh nano -y -qq --no-install-recommends
+
+apt-get update \
+    && apt-get -y -qq --no-install-recommends install build-essential ca-certificates git curl zsh gcc 2>&1 \
+    && apt-get -y -qq clean \
+    && update-ca-certificates \
+    && rm -rf /var/lib/apt/lists/*

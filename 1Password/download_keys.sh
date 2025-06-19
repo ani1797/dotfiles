f5d98ca4abea8cf8a4fc2ca1c11a4452f50f6aef
Anirudh Aggarwal <16053724+ani1797@users.noreply.github.com>
2025-06-19 18:46:31 +0000

    feat: add script to download SSH keys from 1Password and remove allowed signers file
    Signed-off-by: GitHub <noreply@github.com>

---
+++ 1Password/download_keys.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env zsh
+
+set -euo pipefail
+
+download() {
+    required op
+    local vault=$1
+    local download_dir=$2
+
+    local items=$(op item list --categories "SSH Key" --vault "$vault" --format json | jq -r '.[].id')
+    for item in $items; do
+        local name=$(op item get "$item" --vault "$vault" --format json | jq -r '.title')
+        local private_key=$(op read "op://$vault/$item/private_key?ssh-format=openssh")
+        local private_key=$(op read "op://$vault/$item/public_key")
+        local key_type=$(op read "op://$vault/$item/key_type")
+        # Save the private key to a file
+        echo "$private_key" >"${download_dir}/id_${key_type}"
+        echo "$public_key" >"${download_dir}/id_${key_type}.pub"
+        chmod 600 "${download_dir}/id_${key_type}"
+        echo "Downloaded $name to ${download_dir}/id_${key_type}"
+    done
+}
+
+download "${1:-Private}" "${2:-$HOME/.ssh}"
